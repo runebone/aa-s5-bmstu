d726548bde19b1e72c1c27c3bd39771fb549db88
Konstantin Runov <runebone1@gmail.com>
2023-09-05 15:51:06 +0300

    add lev_rec and damlev_iter defs

--- lab_01/algorithm.h
+++ lab_01/algorithm.h
@@ -3,6 +3,8 @@
 
 #include "typedefs.h"
 
+u32 lev_recursive(const char *str1, u32 len1, const char *str2, u32 len2);
 u32 lev_iterative(const char *str1, u32 len1, const char *str2, u32 len2);
+u32 damlev_iterative_with_matrix(const char *str1, u32 len1, const char *str2, u32 len2);
 
 #endif // __ALGORITHM_H__

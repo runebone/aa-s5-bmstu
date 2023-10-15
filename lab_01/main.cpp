f87daf2261dcbcea6fb406f6d58a1835c7eb704d
Konstantin Runov <runebone1@gmail.com>
2023-10-15 13:05:04 +0300

    tracing finally works; stuff

--- lab_01/main.cpp
+++ lab_01/main.cpp
@@ -8,6 +8,7 @@
 #define BUFSIZE 255
 
 void pm(size_t **matrix, size_t r, size_t c);
+void print_matrix_and_strings(size_t **matrix, size_t r, size_t c, const wchar_t *s1, const wchar_t *s2);
 
 // Helper functions
 int input_strings(wchar_t *s1, wchar_t *s2)
@@ -22,8 +23,8 @@ int input_strings(wchar_t *s1, wchar_t *s2)
     }
     else
     {
-        s1[0] = L' ';
-        s2[0] = L' ';
+        s1[0] = L'λ';
+        s2[0] = L'λ';
     }
 
     return rc;
@@ -66,33 +67,88 @@ void free_matrix(size_t **matrix, size_t *first_row)
     free(matrix);
 }
 
-void matrix_roll_up(size_t **matrix, size_t n_rows)
+void matrix_roll_one_up(size_t **matrix, size_t matrix_size_in_rows)
 {
+    size_t n_rows = matrix_size_in_rows;
     size_t *tmp_row = matrix[0];
     size_t first_row_number = matrix[0][0];
-
     for (size_t i = 0; i < n_rows - 1; i++)
     {
         matrix[i] = matrix[i + 1];
     }
-
     // 0 1 2 3 4 5 6 7 8
     // 1 0 0 0 0 0 0 0 0
     // n_rows: 2
     // should be after roll:
     // 1 0 0 0 0 0 0 0 0
     // 2 1 2 3 4 5 6 7 8
-    // so, n_rows + first_row_number
+    // so, first_row_number + n_rows
     matrix[n_rows - 1] = tmp_row;
-    matrix[n_rows - 1][0] = n_rows + first_row_number;
+    matrix[n_rows - 1][0] = first_row_number + n_rows;
+}
+
+void matrix_roll_one_down(size_t **matrix, size_t matrix_size_in_rows)
+{
+    size_t n_rows = matrix_size_in_rows;
+    size_t *tmp_row = matrix[n_rows - 1];
+    size_t last_row_number = matrix[n_rows - 1][0];
+    for (size_t i = n_rows - 1; i > 0; i--)
+    {
+        matrix[i] = matrix[i - 1];
+    }
+    // 1 1 2 3 4 5 6 7 8
+    // 2 0 0 0 0 0 0 0 0
+    // n_rows: 2
+    // should be after roll:
+    // 0 0 0 0 0 0 0 0 0
+    // 1 1 2 3 4 5 6 7 8
+    // so, last_row_number - n_rows
+    matrix[0] = tmp_row;
+    matrix[0][0] = last_row_number - n_rows;
 }
 
 size_t *min3(size_t *a, size_t *b, size_t *c)
 {
     size_t *result;
+    size_t min = std::min(*a, std::min(*b, *c));
 
-    if (*a <= *b) { if (*a <= *c) { result = a; } else { result = c; } }
-    else { if (*b <= *c) { result = b; } else { result = c; } }
+    if (min == *a)
+    {
+        result = a;
+    }
+    else if (min == *b)
+    {
+        result = b;
+    }
+    else
+    {
+        result = c;
+    }
+
+    return result;
+}
+
+size_t *min4(size_t *a, size_t *b, size_t *c, size_t *d)
+{
+    size_t *result;
+    size_t min = std::min(std::min(*a, *b), std::min(*c, *d));
+
+    if (min == *a)
+    {
+        result = a;
+    }
+    else if (min == *b)
+    {
+        result = b;
+    }
+    else if (min == *c)
+    {
+        result = c;
+    }
+    else
+    {
+        result = d;
+    }
 
     return result;
 }
@@ -116,24 +172,96 @@ int levenshtein_iterative_matrix(const wchar_t *s1, const wchar_t *s2)
     {
         for (size_t j = 1; j < len2; j++)
         {
-            /* insert_cost = matrix[i - 1][j] + 1; */
-            /* delete_cost = matrix[i][j - 1] + 1; */
             insert_cost = matrix[1 - 1][j] + 1;
             delete_cost = matrix[1][j - 1] + 1;
             cond = (s1[i] == s2[j]);
-            /* replace_cost = matrix[i - 1][j - 1] + (cond ? 0 : 1); */
             replace_cost = matrix[1 - 1][j - 1] + (cond ? 0 : 1);
             who = min3(&insert_cost, &delete_cost, &replace_cost);
-            /* matrix[i][j] = *who; */
             matrix[1][j] = *who;
         }
-
-        matrix_roll_up(matrix, 2);
+        matrix_roll_one_up(matrix, 2);
     }
 
     wprintf(L"%d\n", matrix[0][len2 - 1]);
 
     free_matrix(matrix, first_row);
+
+    return 0;
+}
+
+int levenshtein_iterative_full_matrix(const wchar_t *s1, const wchar_t *s2)
+{
+    size_t len1 = wcsnlen(s1, BUFSIZE); // n_rows
+    size_t len2 = wcsnlen(s2, BUFSIZE); // n_columns
+
+    size_t **matrix = create_matrix(len1, len2);
+
+    if (matrix == NULL) return -1;
+
+    size_t insert_cost, delete_cost, replace_cost;
+    bool cond;
+    size_t *who;
+
+    for (size_t i = 1; i < len1; i++)
+    {
+        for (size_t j = 1; j < len2; j++)
+        {
+            insert_cost = matrix[i - 1][j] + 1;
+            delete_cost = matrix[i][j - 1] + 1;
+            cond = (s1[i] == s2[j]);
+            replace_cost = matrix[i - 1][j - 1] + (cond ? 0 : 1);
+            who = min3(&insert_cost, &delete_cost, &replace_cost);
+            matrix[i][j] = *who;
+        }
+    }
+
+    wprintf(L"%d\n", matrix[len1 - 1][len2 - 1]);
+
+    // Get trace
+    std::vector<char> trace;
+    int count = 0;
+    for (int i = len1 - 1, j = len2 - 1; (i + j) > 0;)
+    {
+        size_t left = (j > 0) ? matrix[i][j - 1] : -1;
+        size_t diag = (i > 0 && j > 0) ? matrix[i - 1][j - 1] : -1;
+        size_t up = (i > 0) ? matrix[i - 1][j] : -1;
+
+        who = min3(&left, &diag, &up);
+
+        if (*who == diag)
+        {
+            if (matrix[i][j] == diag)
+            {
+                trace.push_back('M');
+            }
+            else
+            {
+                trace.push_back('R');
+            }
+            --i;
+            --j;
+        }
+        else if (*who == left)
+        {
+            trace.push_back('I');
+            --j;
+        }
+        else
+        {
+            trace.push_back('D');
+            --i;
+        }
+        ++count;
+    }
+    for (size_t i = 0; i < trace.size(); i++)
+    {
+        wprintf(L"%lc", trace.at(trace.size() - 1 - i));
+    }
+    wprintf(L"\n%d\n", count);
+
+    print_matrix_and_strings(matrix, len1, len2, s1, s2);
+
+    free_matrix(matrix, matrix[0]);
 
     return 0;
 }
@@ -196,6 +324,7 @@ int main()
             rc = input_strings(s1, s2);
 
             levenshtein_iterative_matrix(s1, s2);
+            levenshtein_iterative_full_matrix(s1, s2);
         }
         else if (c == 2)
         {
@@ -225,7 +354,26 @@ void pm(size_t **matrix, size_t r, size_t c)
     {
         for (size_t j = 0; j < c; j++)
         {
-            wprintf(L"%d ", matrix[i][j]);
+            wprintf(L"%2d ", matrix[i][j]);
+        }
+        wprintf(L"\n");
+    }
+}
+
+void print_matrix_and_strings(size_t **matrix, size_t r, size_t c, const wchar_t *s1, const wchar_t *s2)
+{
+    wprintf(L"%2lc ", ' ');
+    for (size_t j = 0; j < c; j++)
+    {
+        wprintf(L"%2lc ", s2[j]);
+    }
+    wprintf(L"\n");
+    for (size_t i = 0; i < r; i++)
+    {
+        wprintf(L"%2lc ", s1[i]);
+        for (size_t j = 0; j < c; j++)
+        {
+            wprintf(L"%2d ", matrix[i][j]);
         }
         wprintf(L"\n");
     }

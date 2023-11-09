b66d4b1535d7b7a4144aae4039d314ec4f7ded1d
Konstantin Runov <runebone1@gmail.com>
2023-11-09 16:23:10 +0300

    bubble sort done

--- lab_03/main.cpp
+++ lab_03/main.cpp
@@ -8,14 +8,12 @@
 
 int main()
 {
-    /* std::vector<int> vec{3, 6, 5, 7, 4, 1, 8, 2, 12, 54, 754, 34, 0, -1, 8, 38}; */
-    std::vector<wchar_t> vec{L'c', L'f', L'e', L'g', L'd', L'a', L'h', L'b'};
+    std::vector<int> vec{3, 6, 5, 7, 4, 1, 8, 2, 12, 54, 754, 34, 0, -1, 8, 38};
 
-    bitonic_sort(&vec[0], vec.size(), sizeof(vec[0]), inverse(asc<decltype(vec[0])>));
-    /* bitonic_merge(&vec[0], vec.size(), sizeof(int), desc); */
+    /* bitonic_sort(&vec[0], vec.size(), sizeof(vec[0]), asc<decltype(vec[0])>); */
+    bubble_sort(&vec[0], vec.size(), sizeof(vec[0]), asc<decltype(vec[0])>);
 
     for (auto v : vec)
-        /* printf("%d ", v); */
-        wprintf(L"%lc ", v);
-    /* printf("\n"); */
+        printf("%d ", v);
+    printf("\n");
 }
--- lab_03/sort_bubble.cpp
+++ lab_03/sort_bubble.cpp
@@ -2,5 +2,19 @@
 
 void bubble_sort(void *base, size_t nmemb, size_t size, cmp_fn_t cmp)
 {
+    for (size_t i = 0; i < nmemb; i++) {
+        bool swapped = false;
+        for (size_t j = 0; j < nmemb - i - 1; j++) {
+            char *a = (char*)base + size * j;
+            char *b = (char*)base + size * (j + 1);
+            if (cmp(a, b)) {
+                swap(a, b, size);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
 }
 

ff68fdbe98a545551a6b6e2479a80f51b5d30dbf
Konstantin Runov <runebone1@gmail.com>
2023-11-10 00:39:16 +0300

    radix works for elements where msb <= 7 (one byte)

--- lab_03/main.cpp
+++ lab_03/main.cpp
@@ -8,10 +8,16 @@
 
 int main()
 {
-    std::vector<int> vec{3, 6, 5, 7, 4, 1, 8, 2, 12, 54, 754, 34, 0, -1, 8, 38};
+    /* std::vector<int> vec{3, 6, 5, 7, 4, 1, 8, 2, 12, 54, 754, 34, 0, -1, 8, 38}; */
+    std::vector<int> vec{3, 6, 5, 255, 4, 1, 12, 2, 32, 0, 8};
+    /* std::vector<int> vec{3, 6, 5, 7, 4, 1, 8, 2, 18}; */
+    /* std::vector<int> vec{3, 6, 5, 7, 4, 1, 8, 2}; */
+    /* std::vector<int> vec{1, 3, 2}; */
 
     /* bitonic_sort(&vec[0], vec.size(), sizeof(vec[0]), asc<decltype(vec[0])>); */
-    bubble_sort(&vec[0], vec.size(), sizeof(vec[0]), asc<decltype(vec[0])>);
+    /* bubble_sort(&vec[0], vec.size(), sizeof(vec[0]), asc<decltype(vec[0])>); */
+    /* radix_sort(&vec[0], vec.size(), sizeof(vec[0]), inverse(asc<decltype(vec[0])>)); */
+    radix_sort(&vec[0], vec.size(), sizeof(vec[0]), (asc<decltype(vec[0])>));
 
     for (auto v : vec)
         printf("%d ", v);
--- lab_03/sort_radix.cpp
+++ lab_03/sort_radix.cpp
@@ -5,9 +5,9 @@
 
 typedef unsigned char byte;
 
-unsigned int get_msbit_idx(byte x)
+size_t get_msbit_idx(byte x)
 {
-    unsigned int msbit_idx = 7;
+    size_t msbit_idx = 7;
 
     while ((x & (1 << msbit_idx)) == 0)
         msbit_idx--;
@@ -15,7 +15,7 @@ unsigned int get_msbit_idx(byte x)
     return msbit_idx;
 }
 
-unsigned int find_msbit(void *base, size_t nmemb, size_t size)
+size_t find_msbit(void *base, size_t nmemb, size_t size)
 {
     byte byte_ptr = size - 1;
     byte sum = 0;
@@ -35,15 +35,81 @@ unsigned int find_msbit(void *base, size_t nmemb, size_t size)
     if (byte_ptr < 0)
         return -1; // All elements = 0
 
-    unsigned int msbit_idx = 8 * byte_ptr + get_msbit_idx(sum);
+    size_t msbit_idx = 8 * byte_ptr + get_msbit_idx(sum);
 
     return msbit_idx;
 }
 
 void radix_sort(void *base, size_t nmemb, size_t size, cmp_fn_t cmp)
 {
-    unsigned int msbit_idx = find_msbit(base, nmemb, size);
+    size_t msbit_idx = find_msbit(base, nmemb, size);
+    size_t byte_ptr = 0;
 
-    printf("%u\n", msbit_idx);
+    void *zero_elems[nmemb];
+    void *one_elems[nmemb];
+
+    size_t zero_ptr = 0;
+    size_t zero_cnt = 0;
+    size_t one_ptr = 0;
+    size_t one_cnt = 0;
+    size_t ptr = 0;
+
+    byte a[size], b[size];
+    memset(a, 0, size);
+    memset(b, 255, size);
+    int order_asc = cmp(a, b);
+
+    byte base_copy[nmemb * size];
+    memcpy(base_copy, base, nmemb * size);
+
+    byte mask = 1;
+
+    for (size_t curbit = 0; curbit <= msbit_idx; curbit++, mask <<= 1) {
+        zero_ptr = 0;
+        zero_cnt = 0;
+        one_ptr = 0;
+        one_cnt = 0;
+        ptr = 0;
+
+        if (curbit && (curbit % 8) == 0) {
+            mask = 1;
+            byte_ptr++;
+        }
+
+        for (size_t i = 0; i < nmemb; i++) {
+            byte *elem = (byte*)base + size * i;
+            elem += byte_ptr;
+
+            // HACK Equivalent to (*elem & mask) when order_asc = 1, but when
+            // order_asc = 0, we add ones to zero array to account for order
+            // in the future, avoiding dublicating code.
+            if (((*elem & mask) > 0) ^ !order_asc) {
+                one_elems[one_ptr] = elem;
+                one_ptr++;
+            } else {
+                zero_elems[zero_ptr] = elem;
+                zero_ptr++;
+            }
+        }
+
+        for (; zero_ptr; zero_ptr--) {
+            void *dst = (byte*)base_copy + size * ptr;
+            void *src = zero_elems[zero_cnt];
+            memcpy(dst, src, size);
+            zero_cnt++;
+            ptr++;
+        }
+        for (; one_ptr; one_ptr--) {
+            void *dst = (byte*)base_copy + size * ptr;
+            void *src = one_elems[one_cnt];
+            memcpy(dst, src, size);
+            one_cnt++;
+            ptr++;
+        }
+
+        memcpy(base, base_copy, nmemb * size);
+    }
+
+    printf("%lu\n", msbit_idx);
 }
 
